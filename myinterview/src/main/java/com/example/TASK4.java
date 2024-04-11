3e9f8b35e1ca7ae1d40756b7c136d6266a7d0272
Vinikot <vini.kotches@gmail.com>
2024-04-10 22:33:10 -0300

    TASK 4 Init.

--- myinterview/src/main/java/com/example/TASK4.java
+++ myinterview/src/main/java/com/example/TASK4.java
@@ -1,5 +1,19 @@
 package com.example;
 
+import com.example.task4.model.EmployeeModel;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import org.json.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Create an implementation of a Rest API client.
  * Prints out how many records exists for each gender and save this file to s3 bucket
@@ -9,4 +23,38 @@ package com.example;
  */
 public class TASK4 {
 
+    public static ArrayList<EmployeeModel> getEmplyeeFromAPI() throws ClientProtocolException, IOException {
+
+        ArrayList<EmployeeModel> employeeList = new ArrayList<EmployeeModel>();
+
+        HttpGet request = new HttpGet("https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda");
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling().build();
+
+             CloseableHttpResponse response = httpClient.execute(request)) {
+
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+
+                String result = EntityUtils.toString(entity);
+
+                JSONArray payload = new JSONArray(result);
+
+                for (int i = 0; i < payload.length(); ++i) {
+
+                    EmployeeModel employee = new EmployeeModel();
+
+                    JSONObject dataNome = payload.getJSONObject(i);
+                    frd.setDate(dataNome.getInt("date"));
+                    frd.setName(dataNome.getString("name"));
+
+                    feriado.add(frd);
+                }
+
+            }
+        }
+
+        return feriado;
+    }
 }
---
+++ myinterview/src/main/java/com/example/task4/model/EmployeeModel.java
@@ -0,0 +1,40 @@
+package com.example.task4.model;
+
+public class EmployeeModel {
+    private int emp_no;
+    private String first_name;
+    private String last_name;
+    private char gender;
+
+    public int getEmp_no() {
+        return emp_no;
+    }
+
+    public void setEmp_no(int emp_no) {
+        this.emp_no = emp_no;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
